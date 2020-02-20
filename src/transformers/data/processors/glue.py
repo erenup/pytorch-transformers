40098807bcbac684b984383f3cdecc06fcb63f38
erenup <ping.nie@pku.edu.cn>
2020-02-20 23:28:51 +0800

    add multiple processing

--- src/transformers/data/processors/glue.py
+++ src/transformers/data/processors/glue.py
@@ -20,13 +20,64 @@ import os
 
 from ...file_utils import is_tf_available
 from .utils import DataProcessor, InputExample, InputFeatures
-
+from multiprocessing import Pool, cpu_count
+from functools import partial
+from tqdm import tqdm
 
 if is_tf_available():
     import tensorflow as tf
 
 logger = logging.getLogger(__name__)
 
+def glue_convert_examples_to_feature(
+    example,
+    tokenizer,
+    max_length=512,
+    task=None,
+    label_list=None,
+    output_mode=None,
+    pad_on_left=False,
+    pad_token=0,
+    pad_token_segment_id=0,
+    mask_padding_with_zero=True,
+):
+    label_map = {label: i for i, label in enumerate(label_list)}
+    inputs = tokenizer.encode_plus(example.text_a, example.text_b, add_special_tokens=True, max_length=max_length, )
+    input_ids, token_type_ids = inputs["input_ids"], inputs["token_type_ids"]
+
+    # The mask has 1 for real tokens and 0 for padding tokens. Only real
+    # tokens are attended to.
+    attention_mask = [1 if mask_padding_with_zero else 0] * len(input_ids)
+
+    # Zero-pad up to the sequence length.
+    padding_length = max_length - len(input_ids)
+    if pad_on_left:
+        input_ids = ([pad_token] * padding_length) + input_ids
+        attention_mask = ([0 if mask_padding_with_zero else 1] * padding_length) + attention_mask
+        token_type_ids = ([pad_token_segment_id] * padding_length) + token_type_ids
+    else:
+        input_ids = input_ids + ([pad_token] * padding_length)
+        attention_mask = attention_mask + ([0 if mask_padding_with_zero else 1] * padding_length)
+        token_type_ids = token_type_ids + ([pad_token_segment_id] * padding_length)
+
+    assert len(input_ids) == max_length, "Error with input length {} vs {}".format(len(input_ids), max_length)
+    assert len(attention_mask) == max_length, "Error with input length {} vs {}".format(
+        len(attention_mask), max_length
+    )
+    assert len(token_type_ids) == max_length, "Error with input length {} vs {}".format(
+        len(token_type_ids), max_length
+    )
+
+    if output_mode == "classification":
+        label = label_map[example.label]
+    elif output_mode == "regression":
+        label = float(example.label)
+    else:
+        raise KeyError(output_mode)
+
+    return  InputFeatures(
+            input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids, label=label
+        )
 
 def glue_convert_examples_to_features(
     examples,
@@ -79,61 +130,25 @@ def glue_convert_examples_to_features(
     label_map = {label: i for i, label in enumerate(label_list)}
 
     features = []
-    for (ex_index, example) in enumerate(examples):
-        len_examples = 0
-        if is_tf_dataset:
-            example = processor.get_example_from_tensor_dict(example)
-            example = processor.tfds_map(example)
-            len_examples = tf.data.experimental.cardinality(examples)
-        else:
-            len_examples = len(examples)
-        if ex_index % 10000 == 0:
-            logger.info("Writing example %d/%d" % (ex_index, len_examples))
 
-        inputs = tokenizer.encode_plus(example.text_a, example.text_b, add_special_tokens=True, max_length=max_length,)
-        input_ids, token_type_ids = inputs["input_ids"], inputs["token_type_ids"]
-
-        # The mask has 1 for real tokens and 0 for padding tokens. Only real
-        # tokens are attended to.
-        attention_mask = [1 if mask_padding_with_zero else 0] * len(input_ids)
-
-        # Zero-pad up to the sequence length.
-        padding_length = max_length - len(input_ids)
-        if pad_on_left:
-            input_ids = ([pad_token] * padding_length) + input_ids
-            attention_mask = ([0 if mask_padding_with_zero else 1] * padding_length) + attention_mask
-            token_type_ids = ([pad_token_segment_id] * padding_length) + token_type_ids
-        else:
-            input_ids = input_ids + ([pad_token] * padding_length)
-            attention_mask = attention_mask + ([0 if mask_padding_with_zero else 1] * padding_length)
-            token_type_ids = token_type_ids + ([pad_token_segment_id] * padding_length)
-
-        assert len(input_ids) == max_length, "Error with input length {} vs {}".format(len(input_ids), max_length)
-        assert len(attention_mask) == max_length, "Error with input length {} vs {}".format(
-            len(attention_mask), max_length
+    with Pool(cpu_count()) as p:
+        annotate_ = partial(
+            glue_convert_examples_to_feature,
+            tokenizer=tokenizer,
+            max_length=max_length,
+            task=task,
+            label_list=label_list,
+            output_mode=output_mode,
+            pad_on_left=pad_on_left,
+            pad_token=pad_token,
+            pad_token_segment_id=pad_token_segment_id,
+            mask_padding_with_zero=mask_padding_with_zero,
         )
-        assert len(token_type_ids) == max_length, "Error with input length {} vs {}".format(
-            len(token_type_ids), max_length
-        )
-
-        if output_mode == "classification":
-            label = label_map[example.label]
-        elif output_mode == "regression":
-            label = float(example.label)
-        else:
-            raise KeyError(output_mode)
-
-        if ex_index < 5:
-            logger.info("*** Example ***")
-            logger.info("guid: %s" % (example.guid))
-            logger.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
-            logger.info("attention_mask: %s" % " ".join([str(x) for x in attention_mask]))
-            logger.info("token_type_ids: %s" % " ".join([str(x) for x in token_type_ids]))
-            logger.info("label: %s (id = %d)" % (example.label, label))
-
-        features.append(
-            InputFeatures(
-                input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids, label=label
+        features = list(
+            tqdm(
+                p.imap(annotate_, examples, chunksize=32),
+                total=len(examples),
+                desc="convert {} examples".format(task),
             )
         )
 
--- src/transformers/data/processors/squad.py
+++ src/transformers/data/processors/squad.py
@@ -414,6 +414,45 @@ def squad_convert_examples_to_features(
     return features
 
 
+def convert2squad_example(entry, is_training=False):
+    title = entry["title"]
+    examples = []
+    for paragraph in entry["paragraphs"]:
+        context_text = paragraph["context"]
+        for qa in paragraph["qas"]:
+            qas_id = qa["id"]
+            question_text = qa["question"]
+            start_position_character = None
+            answer_text = None
+            answers = []
+
+            if "is_impossible" in qa:
+                is_impossible = qa["is_impossible"]
+            else:
+                is_impossible = False
+
+            if not is_impossible:
+                if is_training:
+                    answer = qa["answers"][0]
+                    answer_text = answer["text"]
+                    start_position_character = answer["answer_start"]
+                else:
+                    answers = qa["answers"]
+
+            example = SquadExample(
+                qas_id=qas_id,
+                question_text=question_text,
+                context_text=context_text,
+                answer_text=answer_text,
+                start_position_character=start_position_character,
+                title=title,
+                is_impossible=is_impossible,
+                answers=answers,
+            )
+            examples.append(example)
+    return examples
+
+
 class SquadProcessor(DataProcessor):
     """
     Processor for the SQuAD data set.
@@ -524,42 +563,19 @@ class SquadProcessor(DataProcessor):
     def _create_examples(self, input_data, set_type):
         is_training = set_type == "train"
         examples = []
-        for entry in tqdm(input_data):
-            title = entry["title"]
-            for paragraph in entry["paragraphs"]:
-                context_text = paragraph["context"]
-                for qa in paragraph["qas"]:
-                    qas_id = qa["id"]
-                    question_text = qa["question"]
-                    start_position_character = None
-                    answer_text = None
-                    answers = []
-
-                    if "is_impossible" in qa:
-                        is_impossible = qa["is_impossible"]
-                    else:
-                        is_impossible = False
-
-                    if not is_impossible:
-                        if is_training:
-                            answer = qa["answers"][0]
-                            answer_text = answer["text"]
-                            start_position_character = answer["answer_start"]
-                        else:
-                            answers = qa["answers"]
-
-                    example = SquadExample(
-                        qas_id=qas_id,
-                        question_text=question_text,
-                        context_text=context_text,
-                        answer_text=answer_text,
-                        start_position_character=start_position_character,
-                        title=title,
-                        is_impossible=is_impossible,
-                        answers=answers,
-                    )
-
-                    examples.append(example)
+        with Pool(cpu_count()) as p:
+            annotate_ = partial(
+                convert2squad_example,
+                is_training=is_training,
+            )
+            exampls_multi = list(
+                tqdm(
+                    p.imap(annotate_, input_data, chunksize=32),
+                    total=len(input_data),
+                    desc="convert squad examples",
+                )
+            )
+            examples = [x for single in exampls_multi for x in single]
         return examples
 
 
